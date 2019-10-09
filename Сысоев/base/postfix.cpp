58746e8cdc0a7598a61cdd60bae1cd2f69ebb9bc
Ilya Kochankov <fakafafakafa@mail.ru>
2019-10-09 18:14:47 +0300

    Написан стек и тесты к нему

--- Сысоев/base/postfix.cpp
+++ Сысоев/base/postfix.cpp
@@ -1,13 +1,40 @@
 ﻿#include "postfix.h"
-#include "stack.h"
+#include <stdexcept>
 
-string TPostfix::ToPostfix()
+void TPostfix::ToPostfix()
 {
-  postfix = string("ab+");
-  return postfix;
 }
 
 double TPostfix::Calculate()
 {
-  return 0;
+	return 0.0;
+}
+
+string TPostfix::getTableBrackets()
+{
+	return string();
+}
+
+void TPostfix::isCorrect()
+{
+	TStack<int> stack;
+	for (int i = 0; i < infix.length(); i++) {
+		switch (infix[i])
+		{
+		case '(':
+			stack.push(i);
+			break;
+		case ')':
+			if (stack.isEmpty()) {
+				throw std::runtime_error("Выражение заданно неверно! Закрывающая скобка идет до открывающей в " + std::to_string(i) + " позиции.");
+			}
+			stack.pop();
+			break;
+		default:
+			continue;
+		}
+		if (!stack.isEmpty()) {
+			throw std::runtime_error("Выражение заданно неверно! Скобк" + (stack.top > 0? "и":"а") + " не закрыт" + (stack.top > 0 ? "ы!" : "а!"));
+		}
+	}
 }
